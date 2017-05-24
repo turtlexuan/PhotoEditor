0cce094abd3f69f77f8f61c5d93ef9bf9d1f3794
turtlexuan <turtlexuan@gmail.com>
2017-05-24 16:50:03 +0800

    1. Create Project. 2. Pod Install

---
+++ PhotoEditor/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PhotoEditor
+//
+//  Created by 劉仲軒 on 2017/5/24.
+//  Copyright © 2017年 劉仲軒. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PhotoEditor/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PhotoEditor
+//
+//  Created by 劉仲軒 on 2017/5/24.
+//  Copyright © 2017年 劉仲軒. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
